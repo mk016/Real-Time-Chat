c86a6f3a3eab1b01813f4e4f4055fabd198f68c9
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-13 12:12:18 +0000

    update

---
+++ Real-Time-Chat/server.js
@@ -0,0 +1,8 @@
+const express = require ("express");
+const path = require("Path");
+
+const app = express();
+const server = require("http").createServer(app);
+
+app.use(express.static(path.join(__dirname+"/public")));
+server.listen(5000)
